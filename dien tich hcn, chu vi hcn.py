4dc9510809503c86f35fa89c115fd6ba2c336fab
tuent67khmt <tue.nt.67khmt@ntu.edu.vn>
2025-11-16 13:26:33 +0700

    diện tích, chu vi hình chữ nhật

---
+++ dien tich hcn, chu vi hcn.py
@@ -0,0 +1,8 @@
+def dien_tich_hcn(a,b):
+    return a*b
+def chu_vi_hcn(a,b):
+    return (a+b)*2
+a=int(input('Nhập số a'))
+b=int(input('Nhập số b'))
+print('Chu vi hình chữ nhật là',chu_vi_hcn(a,b))
+print('Diện tích hình chữ nhật là',dien_tich_hcn(a,b))
