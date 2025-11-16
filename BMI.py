b413b54e49d4a64a57bd801393748e742cbe0b3b
tuent67khmt <tue.nt.67khmt@ntu.edu.vn>
2025-11-16 13:58:13 +0700

    BMI

---
+++ BMI.py
@@ -0,0 +1,8 @@
+def tinh_BMI(a,b):
+    return(a,b)
+a=int(input('nhập cân nặng của bạn '))
+print('cân nặng của bạn ',a,'kg')
+b=float(input('nhập chiều cao của bạn '))
+print(' chiều cao của bạn ',b,'m')
+BMI = a / (b*b)
+print('Chỉ số BMI của bạn là: ',BMI)
